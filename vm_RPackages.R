2f40f437fae474745a6336a5b6546e470482482b
Steve Pederson <steveped@users.noreply.github.com>
2018-01-16 15:35:27 +1030

    Update vm_RPackages.R

--- vm_RPackages.R
+++ vm_RPackages.R
@@ -6,7 +6,7 @@ pkgs <- c("reshape2", "ggrepel", "readxl", "AnnotationHub", "biomaRt", "Biostrin
           "Gviz", "ggbio", "Biobase", "edgeR", "limma", "Glimma", "xtable", "pander", "knitr",
           "rmarkdown", "lme4", "multcomp", "scales", "stringr", "corrplot", "pheatmap", "devtools", "tidyverse",
           "BiocGenerics", "BiocStyle", "checkmate", "ggdendro", "lubridate", "magrittr",
-          "plotly", "shiny", "ShortRead", "viridis", "viridisLite", "zoo", "shinyFiles",
-         "UofABioinformaticsHub/ngsReports")
+          "plotly", "shiny", "ShortRead", "viridis", "viridisLite", "zoo", "shinyFiles")
 biocLite(pkgs)
+biocLite("UofABioinformaticsHub/ngsReports")
 biocValid(fix = TRUE)
