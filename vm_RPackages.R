468e152903edb1cfee09408930d5c475559cfcc8
Steve Pederson <steveped@users.noreply.github.com>
2018-01-16 15:18:07 +1030

    Update vm_RPackages.R

--- vm_RPackages.R
+++ vm_RPackages.R
@@ -1,10 +1,12 @@
 #!/usr/bin/Rscript
 
 source("https://bioconductor.org/biocLite.R")
-pkgs <- c("dplyr","reshape2","ggplot2","ggrepel","readr","readxl","tibble","AnnotationHub",
-          "biomaRt","Biostrings","BSgenome","DESeq2","GenomicRanges","GenomicFeatures",
-          "Rsubread","Rsamtools","rtracklayer","Gviz","ggbio","Biobase","edgeR","limma",
-          "Glimma","xtable","pander","knitr","rmarkdown","lme4","multcomp","scales","stringr",
-          "corrplot","pheatmap","devtools", "tidyverse")
+pkgs <- c("reshape2", "ggrepel", "readxl", "AnnotationHub", "biomaRt", "Biostrings", "BSgenome",
+          "DESeq2", "GenomicRanges", "GenomicFeatures", "Rsubread", "Rsamtools", "rtracklayer",
+          "Gviz", "ggbio", "Biobase", "edgeR", "limma", "Glimma", "xtable", "pander", "knitr",
+          "rmarkdown", "lme4", "multcomp", "scales", "stringr", "corrplot", "pheatmap", "devtools", "tidyverse",
+          "BiocGenerics", "BiocStyle", "checkmate", "ggdendro", "lubridate", "magrittr",
+          "plotly", "shiny", "ShortRead", "viridis", "viridisLite", "zoo", "shinyFiles",
+         "UofABioinformaticsHub/ngsReports")
 biocLite(pkgs)
 biocValid(fix = TRUE)
